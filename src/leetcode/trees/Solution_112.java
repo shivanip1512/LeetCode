4391235ba8509d582c8c7e9941f5ed6f1a5caa7c
shivanip1512 <shivanipacharne10@gmail.com>
2024-02-10 23:20:13 +0530

    Problem#112 - target sum in binary tree

---
+++ src/leetcode/trees/Solution_112.java
@@ -0,0 +1,53 @@
+package leetcode.trees;
+import java.util.Stack;
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode() {
+	}
+
+	TreeNode(int val) {
+		this.val = val;
+	}
+
+	TreeNode(int val, TreeNode left, TreeNode right) {
+		this.val = val;
+		this.left = left;
+		this.right = right;
+	}
+}
+
+class Solution {
+	public boolean hasPathSum(TreeNode root, int targetSum) {
+		if (root == null)
+			return false;
+
+		Stack<TreeNode> node_stack = new Stack<TreeNode>();
+		Stack<Integer> curr_sum_left_stack = new Stack<Integer>();
+
+		node_stack.add(root);
+		curr_sum_left_stack.add(targetSum-root.val);
+		
+		while(!node_stack.isEmpty()) {
+			TreeNode treeNode = node_stack.pop();
+			Integer curr_sum = curr_sum_left_stack.pop();
+			
+			if(curr_sum==0 && treeNode.left==null && treeNode.right==null)
+				return true;
+			
+			if(treeNode.left !=null && curr_sum-treeNode.left.val>=0) {
+				node_stack.add(treeNode.left);
+				curr_sum_left_stack.add(curr_sum-treeNode.left.val);
+			}
+			
+			if(treeNode.right !=null && curr_sum-treeNode.right.val>=0) {
+				node_stack.add(treeNode.right);
+				curr_sum_left_stack.add(curr_sum-treeNode.right.val);
+			}
+		}
+		return false;
+	}
+}
