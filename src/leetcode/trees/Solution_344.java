6944c5eafee71777f21c3c587154b0b8575a0d39
shivanip1512 <shivanipacharne10@gmail.com>
2024-11-20 22:40:55 +0530

    Problem#344 : Reverse a String

---
+++ src/leetcode/trees/Solution_344.java
@@ -0,0 +1,16 @@
+package leetcode.trees;
+
+public class Solution_344 {
+
+	public void reverseString(char[] s) {
+		int start = 0;
+		int end = s.length - 1;
+		for (int i = 0; i < s.length / 2; i++) {
+			char temp = s[start];
+			s[start] = s[end];
+			s[end] = temp;
+			start++;
+			end--;
+		}
+	}
+}
