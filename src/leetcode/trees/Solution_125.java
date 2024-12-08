e59c5310432e6576ad31806cff21b64dbcafa016
shivanip1512 <shivanipacharne10@gmail.com>
2024-12-08 14:59:31 +0530

    Problem#125 : Valid Palindrome (Time complexity improved)

--- src/leetcode/trees/Solution_125.java
+++ src/leetcode/trees/Solution_125.java
@@ -8,13 +8,15 @@ public class Solution_125 {
 	}
 
 	public boolean isPalindrome(String s) {
-		s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-		System.out.println(s);
 		int start = 0, end = s.length() - 1;
 		while (start < end) {
-			if (s.charAt(start) != s.charAt(end)) {
+			while (start < end && !Character.isLetterOrDigit(s.charAt(start)))
+				start++;
+			while (start < end && !Character.isLetterOrDigit(s.charAt(end)))
+				end--;
+			if (Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)))
 				return false;
-			}
+
 			start++;
 			end--;
 		}
