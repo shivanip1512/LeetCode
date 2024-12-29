2c1d3f0f756587b51b03789ea646fe7a100b8f53
shivanip1512 <shivanipacharne10@gmail.com>
2024-12-29 15:45:07 +0530

    Sum of Digit in a number

---
+++ src/recursion/SumOfDigits.java
@@ -0,0 +1,15 @@
+package recursion;
+
+public class SumOfDigits {
+
+	public static void main(String[] args) {
+		System.out.println(sumOfDigits(12563));
+	}
+
+	private static int sumOfDigits(int n) {
+		if(n==0)
+			return 0;
+		return n%10 + sumOfDigits(n/10);
+	}
+
+}
