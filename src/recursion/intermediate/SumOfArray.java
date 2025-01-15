c13c47a4175c58f7e2240d94e3d189ec1036eba5
shivanip1512 <SHIVANI@Shivani>
2025-01-16 00:47:57 +0530

    Sum of Array

--- src/recursion/intermediate/SumOfArray.java
+++ src/recursion/intermediate/SumOfArray.java
@@ -7,6 +7,14 @@ public class SumOfArray {
 	public static void main(String[] args) {
 		Integer[] arr = { 1, 2, 3, 4,10};
 		System.out.println(sumArray(arr, arr.length));
+		System.out.println(sumArray2(arr,arr.length,0));
+	}
+
+	private static int sumArray2(Integer[] arr, int length, int i) {
+		if(i==length)
+			return 0;
+		
+		return arr[i]+sumArray2(arr, length, i+1);
 	}
 
 	private static int sumArray(Integer[] arr, int length) {
