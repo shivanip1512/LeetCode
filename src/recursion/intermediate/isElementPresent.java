b8096006fe49e9b6762755f0780cff2d2f4dad7b
shivanip1512 <shivanipacharne10@gmail.com>
2025-01-26 00:57:10 +0530

    Index approach : Simulated with for loop logic

--- src/recursion/intermediate/isElementPresent.java
+++ src/recursion/intermediate/isElementPresent.java
@@ -5,18 +5,27 @@ import java.util.Arrays;
 public class isElementPresent {
 
 	public static void main(String[] args) {
-		int arr[] = {1,3,5,1,9,6};
-		System.out.println(isPresent(arr, arr.length, 10));;
+		int arr[] = { 1, 3, 5, 1, 9, 6 };
+		System.out.println(isPresent(arr, arr.length, 1, 0));
 	}
 
 	private static boolean isPresent(int[] arr, int n, int x) {
-		if(n==0)
+		if (n == 0)
 			return false;
-		
-		if(arr[0]==x)
+
+		if (arr[0] == x)
 			return true;
-		
-		return isPresent(Arrays.copyOfRange(arr, 1, n), n-1, x);
+
+		return isPresent(Arrays.copyOfRange(arr, 1, n), n - 1, x);
 	}
 
+	private static boolean isPresent(int[] arr, int n, int x, int i) {
+		if (i == n)
+			return false;
+
+		if (arr[i] == x)
+			return true;
+
+		return isPresent(arr, n, x, i + 1);
+	}
 }
