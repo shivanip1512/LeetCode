5992c6d41b6feb16e63ad05889f45acf15639e79
shivanip1512 <shivanipacharne10@gmail.com>
2025-02-15 03:37:15 +0530

    second approach

--- src/recursion/intermediate/CountOccurrencesOfElement.java
+++ src/recursion/intermediate/CountOccurrencesOfElement.java
@@ -3,8 +3,19 @@ package recursion.intermediate;
 public class CountOccurrencesOfElement {
 
 	public static void main(String[] args) {
+		int x = 5;
 		int arr[] = { 5, 2, 5, 6, 7, 9, 5 };
-		System.out.println(countElementOccurrence(arr, arr.length, 0, 0, 5));
+		System.out.println(countElementOccurrence(arr, arr.length, 0, 0, x));
+		System.out.println(countElementOccurrence2(arr, arr.length, 0, x));
+	}
+
+	private static int countElementOccurrence2(int[] arr, int n, int i, int x) {
+		if (i == n)
+			return 0;
+		if (arr[i] == x)
+			return 1 + countElementOccurrence2(arr, n, i + 1, x);
+		else
+			return countElementOccurrence2(arr, n, i + 1, x);
 	}
 
 	private static int countElementOccurrence(int[] arr, int n, int i, int ans, int x) {
