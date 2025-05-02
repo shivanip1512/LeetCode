9d0f5b05790f51c7c22e4c3a003c26ac2bc8750a
shivanip1512 <shivanipacharne10@gmail.com>
2025-05-03 00:30:50 +0530

    Added print2D matrix method in utility

--- src/leetcode/trees/Solution_48.java
+++ src/leetcode/trees/Solution_48.java
@@ -1,20 +1,13 @@
 package leetcode.trees;
 
+import util.UdemyUtil;
+
 public class Solution_48 {
 
 	public static void main(String[] args) {
 		int [][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
 		rotate(matrix);
-		print2DArray(matrix);
-	}
-
-	private static void print2DArray(int[][] matrix) {
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[i].length; j++) {
-				System.out.print(matrix[i][j]+" ");
-			}
-			System.out.println();
-		}
+		UdemyUtil.print2DArray(matrix);
 	}
 
 	public static void rotate(int[][] matrix) {
--- src/util/UdemyUtil.java
+++ src/util/UdemyUtil.java
@@ -47,4 +47,13 @@ public class UdemyUtil {
 		}
 		return arr;
 	}
+	
+	public static void print2DArray(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print(matrix[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
 }
