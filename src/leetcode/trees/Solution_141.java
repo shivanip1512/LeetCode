94623bed903a76611900b9442ce87e4c9d623c8b
shivanip1512 <shivanipacharne10@gmail.com>
2025-05-11 18:27:57 +0530

    Problem#141. Linked List Cycle (using slow and fast pointer approach)

---
+++ src/leetcode/trees/Solution_141.java
@@ -0,0 +1,22 @@
+package leetcode.trees;
+
+import leetcode.ListNode.ListNode;
+
+public class Solution_141 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean hasCycle(ListNode head) {
+		ListNode slow = head, fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+			if (slow == fast)
+				return true;
+		}
+		return false;
+	}
+}
