76d90238c2837d32012ef0b41a4eb1819359a910
shivanip1512 <shivanipacharne10@gmail.com>
2025-05-25 18:04:53 +0530

    Problem#105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/leetcode/trees/Solution_105.java
@@ -0,0 +1,41 @@
+package leetcode.trees;
+
+public class Solution_105 {
+	public TreeNode buildTree(int[] preorder, int[] inorder) {
+		if (preorder == null || inorder == null || preorder.length != inorder.length)
+			return null;
+		return buildTreeHelper(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
+	}
+
+	private TreeNode buildTreeHelper(int[] preorder, int[] inorder, int preStartIndex, int preEndIndex,
+			int inStartIndex, int inEndIndex) {
+		if (inStartIndex > inEndIndex)
+			return null;
+
+		int rootVal = preorder[preStartIndex];
+
+		int leftInStartIdx = inStartIndex;
+		int leftInEndIdx = findElement(inorder, rootVal);
+		int leftPreStartIdx = preStartIndex + 1;
+		int leftPreEndIdx = leftPreStartIdx + (leftInEndIdx - leftInStartIdx);
+
+		int rightPreStartIdx = leftPreEndIdx + 1;
+		int rightPreEndIdx = preEndIndex;
+		int rightInStartIdx = leftInEndIdx + 2;
+		int rightInEndIdx = inEndIndex;
+
+		TreeNode root = new TreeNode(rootVal);
+		root.left = buildTreeHelper(preorder, inorder, leftPreStartIdx, leftPreEndIdx, leftInStartIdx, leftInEndIdx);
+		root.right = buildTreeHelper(preorder, inorder, rightPreStartIdx, rightPreEndIdx, rightInStartIdx,
+				rightInEndIdx);
+		return root;
+	}
+
+	private int findElement(int[] inorder, int rootVal) {
+		for (int i = 0; i < inorder.length; i++) {
+			if (inorder[i] == rootVal)
+				return i - 1;
+		}
+		return 0;
+	}
+}
