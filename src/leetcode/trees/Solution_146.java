548c9946740509273cea718a6b9d01e95b8d7f2e
shivanip1512 <shivanipacharne10@gmail.com>
2025-06-15 03:42:51 +0530

    Problem#146 : LRU implementation

---
+++ src/leetcode/trees/Solution_146.java
@@ -0,0 +1,34 @@
+package leetcode.trees;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+class Solution_146 {
+    private int capacity;
+    private LinkedHashMap<Integer, Integer> map;
+
+    public Solution_146(int capacity) {
+        this.capacity = capacity;
+        this.map = new LinkedHashMap<>(capacity, 0.75f, true) {
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > Solution_146.this.capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return map.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        map.put(key, value);
+    }
+}
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
